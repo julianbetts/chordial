fcfd125d138c57b95d5ed8638777590d2be0eac7
Julian Betts <wjulianbetts@gmail.com>
2025-08-29 14:34:51 -0700

    conditional for if statmenet in diagram.svelte

--- chordial/src/lib/components/Diagram.svelte
+++ chordial/src/lib/components/Diagram.svelte
@@ -15,22 +15,117 @@ export let chord;
 <div>
     <div>
         <div class="fretboard-container">
-            <div class="fret-counter"></div>
+            <div class="base-fret">{chord.baseFret}</div>
+            <!-- columns -->
             <div class="string-1"></div>
             <div class="string-2"></div>
             <div class="string-3"></div>
             <div class="string-4"></div>
             <div class="string-5"></div>
             <div class="string-6"></div>
+            <!-- rows -->
+            <div class="fret-1"></div>
+            <div class="fret-2"></div>
+            <div class="fret-3"></div>
+            <div class="fret-4"></div>
+            <div class="fret-5"></div>
+            <!-- fret marker -->
+
+
+             <!-- fretsOnStrings: [3, 3, 2, 0, 1, 0] -->
+
+            {#each chord.fretsOnStrings as marker, i}
+                {#if marker > 0 || null}
+                    <div class="marker-div" 
+                    style=
+                    "grid-column: {i + 2}/{i + 3}; 
+                    grid-row: {marker}/{marker + 1}">
+                    </div>
+                {/if}
+            {/each}
         </div>
     </div>
 </div>
-
 <!-- css -->
  <style>
 .fretboard-container {
     display: grid;
-    grid-template-columns: 1/2fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: 0.5fr repeat(6, 1fr);
+    grid-template-rows: repeat(5, 1fr);
 }
- </style>
+.base-fret {
+    grid-column: 1/2;
+    grid-row: 1/2;
+    padding-left: .25rem;
+}
+.fret-1 {
+  grid-column: 2/8;
+  grid-row: 1/2;
+  border-bottom: 1px solid black;  
+  /* border-top: 1px solid black; */
+
+}
+.fret-2 {
+  grid-column: 2/8;
+  grid-row: 2/3;
+  border-bottom: 1px solid black;
+}
+.fret-3 {
+  grid-column: 2/8;
+  grid-row: 3/4;
+  border-bottom: 1px solid black;
+}
+.fret-4 {
+  grid-column: 2/8;
+  grid-row: 4/5;
+  border-bottom: 1px solid black;
+}
+.fret-5 {
+  grid-column: 2/8;
+  grid-row: 5/6;
+  border-bottom: 1px solid black;
+}
+
+.marker-div {
+  border: dotted blue;
+  border-radius: 50%;
+  background-color: blue;
+}
+.string-1 {
+  grid-column: 2/3;
+  grid-row: 1/6;
+  border-left: 1px solid black;
+}
+
+.string-2 {
+  grid-column: 3/4;
+  grid-row: 1/6;
+  border-left: 1px solid black;
+}
+
+.string-3 {
+  grid-column: 4/5;
+  grid-row: 1/6;
+  border-left: 1px solid black;
+}
+
+.string-4 {
+  grid-column: 5/6;
+  grid-row: 1/6;
+  border-left: 1px solid black;
+}
+
+.string-5 {
+  grid-column: 6/7;
+  grid-row: 1/6;
+  border-left: 1px solid black;
+}
+
+.string-6 {
+  grid-column: 7/8;
+  grid-row: 1/6;
+  border-left: 1px solid black;
+  /* border-right: 1px solid black; */
+}
+
+</style>
--- chordial/src/lib/stores/chords.js
+++ chordial/src/lib/stores/chords.js
@@ -5,35 +5,36 @@ export const chords = writable([
     chordName: "C",
     shape: "C",
     baseFret: 1,
-    fretsOnStrings: [null, 3, 2, 0, 1, 0],
+    fretsOnStrings: [3, 3, 2, 0, 1, 0],
     label: "C-shape C"
-  },
-  {
-    chordName: "C",
-    shape: "A",
-    baseFret: 3,
-    fretsOnStrings: [null, 3, 5, 5, 5, 3],
-    label: "A-shape C"
-  },
-  {
-    chordName: "C",
-    shape: "G",
-    baseFret: 7,
-    fretsOnStrings: [8, 10, 10, 9, 8, 8],
-    label: "G-shape C"
-  },
-  {
-    chordName: "C",
-    shape: "E",
-    baseFret: 8,
-    fretsOnStrings: [8, 10, 10, 9, 8, 8],
-    label: "E-shape C"
-  },
-  {
-    chordName: "C",
-    shape: "D",
-    baseFret: 10,
-    fretsOnStrings: [null, null, 10, 12, 13, 12],
-    label: "D-shape C"
   }
+  // ,
+  // {
+  //   chordName: "C",
+  //   shape: "A",
+  //   baseFret: 3,
+  //   fretsOnStrings: [null, 3, 5, 5, 5, 3],
+  //   label: "A-shape C"
+  // },
+  // {
+  //   chordName: "C",
+  //   shape: "G",
+  //   baseFret: 7,
+  //   fretsOnStrings: [8, 10, 10, 9, 8, 8],
+  //   label: "G-shape C"
+  // },
+  // {
+  //   chordName: "C",
+  //   shape: "E",
+  //   baseFret: 8,
+  //   fretsOnStrings: [8, 10, 10, 9, 8, 8],
+  //   label: "E-shape C"
+  // },
+  // {
+  //   chordName: "C",
+  //   shape: "D",
+  //   baseFret: 10,
+  //   fretsOnStrings: [null, null, 10, 12, 13, 12],
+  //   label: "D-shape C"
+  // }
 ]);
