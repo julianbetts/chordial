f0ced04d7d57761aa7162d918b836526ac9a36ac
Julian Betts <wjulianbetts@gmail.com>
2025-08-29 12:24:29 -0700

    finished diagram.svelte

--- chordial/src/lib/components/Diagram.svelte
+++ chordial/src/lib/components/Diagram.svelte
@@ -5,11 +5,32 @@ export let chord;
 </script>
 
 <!-- template markup -->
+
+<!-- <div> 
+    {#each chord.fretsOnStrings as fretNumber, i} 
+    <div> the fret you play on string {i} is { fretNumber === null ? "X" : fretNumber } </div>
+    {/each} 
+</div> -->
+
 <div>
-this chords label is {chord.label} and its shape is {chord.shape}
+    <div>
+        <div class="fretboard-container">
+            <div class="fret-counter"></div>
+            <div class="string-1"></div>
+            <div class="string-2"></div>
+            <div class="string-3"></div>
+            <div class="string-4"></div>
+            <div class="string-5"></div>
+            <div class="string-6"></div>
+        </div>
+    </div>
 </div>
 
 <!-- css -->
  <style>
-
+.fretboard-container {
+    display: grid;
+    grid-template-columns: 1/2fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+}
  </style>
